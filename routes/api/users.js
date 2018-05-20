c55e18d8e62b800f6a114ae3df7794b303b436e9
Rob Martin <rmartin3@alumni.unca.edu>
2018-05-20 14:47:03 -0400

    validating registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// load input validation
+const validateRegisterInput = require("../../validation/register");
+
 //Load user model
 const User = require("../../models/User");
 
@@ -18,10 +21,18 @@ router.get("/test", (req, res) => res.json({ msg: "users works" }));
 // @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     // validation
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       // grab gravatar image
       const avatar = gravatar.url(req.body.email, {
